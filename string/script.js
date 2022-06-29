f4d688337ba5b0c1a405c346b9c9acd2121998f9
Ahmed Shams <ahmedshams@Ahmeds-MacBook-Air.local>
2022-06-30 00:45:32 +0200

    new updates about strings

---
+++ string/script.js
@@ -0,0 +1,30 @@
+//convert numbers to string
+
+let x = 100;
+console.log(typeof String(100));
+
+//console.log(typeof 100.toString) ==. float point
+console.log(typeof (100).toString());
+console.log(typeof 100..toString());
+
+// console.log(typeof x.toString); ==> it will be fuction 
+console.log(typeof x.toString());
+
+let name = 'Fady';
+console.log(name.repeat(3)); // repeated three times
+
+let names = 'Ahmed Mohamed';
+console.log(names.length); // all letters will counted even the spaces
+console.log(names[0]);
+console.log(names[6]);
+console.log(names[5]);
+console.log(names.indexOf('7'));
+console.log(names.slice(0, 8));
+
+
+
+
+
+
+
+
