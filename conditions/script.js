adbcffe2975caa904408a150056a6ce45543480c
unknown <shamselddin@outlook.com>
2022-07-04 01:07:07 +0200

    more practicing

--- conditions/script.js
+++ conditions/script.js
@@ -1,11 +1,24 @@
-let x = 'Ahmed';
-let y = 'Ahmed';
+// let x = 'Ahmed';
+// let y = 'Ahmed';
 
-console.log(x == y);
+// console.log(x == y);
 
-let askemail = prompt('enter your email');
-let email = 'ahmed@gmail.com';
+// let askemail = prompt('enter your email');
+// let email = 'ahmed@gmail.com';
 
-console.log(askemail.toLowerCase().trim().length == email); // to make it to lowercase if write with capital letter
+// console.log(askemail.toLowerCase().trim().length == email); // to make it to lowercase if write with capital letter
 // trim for deleting spaces if put spaces by defalt
+
+// console.log(4 != 4);
+// console.log(3 != 4);
+
+let product = 'TV-Samsung';
+let size = 50;
+let cost = 10000;
+
+console.log(cost > 12000 || size == 50); 
+// && => and
+// || => or
+
+
 
