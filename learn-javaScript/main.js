4fd674ee258756b574c239afc98bf4245d5b0ed3
Ahmed Shams <ahmedshams@Ahmeds-MacBook-Air.local>
2022-08-01 11:09:12 +0200

    practicing

---
+++ learn-javaScript/main.js
@@ -0,0 +1,43 @@
+// -- output to screen -- 
+
+// window.alert('Hello from Shams');
+
+document.querySelector('h1').style.color = 'Blue';
+
+document.write('<h2>this is JS page</h2>')
+
+
+// -- data types -- 
+console.log('Ahmed Shams');
+console.log(typeof 'Ahmed Shams');
+console.log(typeof 1000);
+console.log(typeof 1000.1);
+console.log(typeof [5, 10, 15, 20]);
+console.log(typeof ['Ahmed', 'Mohamed', 'Nada']);
+console.log(typeof {name: 'Shams', age: '34', country: 'Egypt'});
+console.log(typeof true);
+console.log(typeof false);
+console.log(typeof undefined);
+console.log(typeof null);
+
+
+// -- variables --
+let user = 'Adam',
+    age = 33,
+    country = 'Egypt';
+
+console.log(user);
+console.log(age);
+console.log(country);
+console.log(hello);
+
+hello.innerHTML = 'bye bye';
+
+// -- concaenation --
+let a = 'i love';
+let b = 'javaScript';
+
+document.write(a + ' ' + b);
+
+console.log(a, b);
+
